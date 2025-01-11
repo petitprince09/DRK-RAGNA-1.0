17bdd6ee38d076b958a4f19992d6894e4467c7c3
petitprince09 <lepetitprince098@gmail.com>
2025-01-11 16:45:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vakp0UnIC
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/vRR63CN.jpeg,https://i.imgur.com/ovCfp9J.jpeg,https://i.imgur.com/edzo5dj.jpeg";
 global.devs = "https://wa.me/50931461936 , https://wa.me/50931461936";
 global.sudo = process.env.SUDO || "50931461936";
-global.owner = process.env.OWNER_NUMBER || "PUT YOUR OWNER NUMBER";
+global.owner = process.env.OWNER_NUMBER || "50940812336";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "true";
@@ -43,7 +43,7 @@ global.scan = "https://drk-tech-2.onrender.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "mettre votre session ici"
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicUkrV0tVelVzcFdhZGhEbUVrTlZSa3JYY2k0RVFqNGp0bGRvSEgrOWxYcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiamVsWWR6ZklzZG5EQVhINFNOazExOGxqMjFMRHB5L1lVTE0vaHMvYVlSVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5RTVKNnJQQkhVaVJHa1dESkphLzhzenVxVWs5Nk9HVUpIakd2SVV0N0Y0PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJWaVd5ajZZdmVSckROR0s4dkVReGJkVDUzODBXc2JHbitCbkpZOUJkd2lFPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJDK05YOHBOd1gvQ2Y2b2hxOUhuaC84Y2JxTGNDcVlXWGpjMmk5TXVKbEU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InZzVWJpVTNrTE0wa0pGNit2R2NNTXF6YjZ0WXNTbTM5R0NjZGxDZ2xSeWM9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic0VPKzRwdEwzQkRCdDBZNE9iOUpwM2Y0U0xQMHJUeEVab2h0dmFjcjFtZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiM3h2dm5MWUh6OERCdjhkakJqRDFSNkdid0x0T0swU2RXSmRCZ0txTkVSWT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjR2WWV0QStEd0ordmhZYjBDY2tuQTA2b1pMdDd0QjhiZDB5c2xNSlBuSXluWG9yZnltdmdRaGVxdys0UU56R082eVZKOExNbkNtUithSzYxNkZhWGlRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTI2LCJhZHZTZWNyZXRLZXkiOiJlbGdyb1kxU09yQUozY2RtL1N1K0o4a0c1NngyQUU4OGlKU1VrQ2s4NmxFPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJnOHdZaHhhY1FOLUV2WFM0Y3R6cFVBIiwicGhvbmVJZCI6IjkyZjMwY2EyLWE3NTktNDdjOC1iYjEyLTYxY2Q4ZjY5NDAzNSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjeHJ0Q3Y2T0s1M0dJMnRmZFQxQWd2OUp0Rkk9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQW1zaW92QUtZMTNRTkNpbnBSK0lqcFJWNXlVPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlFCSFo1SDlRIiwibWUiOnsiaWQiOiI1MDk0MDgxMjMzNjoyQHMud2hhdHNhcHAubmV0IiwibmFtZSI6IkxlIFBldGl0IFByaW5jZSJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTm1mai9ZSEVJSExpN3dHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5Ijoic21RSjFKQnZ2eDJNVjg3NkcrQ0VFOHp1eHIxQk0wZEt5UlZTSmtPWHVXOD0iLCJhY2NvdW50U2lnbmF0dXJlIjoiaDRFc3JHVHNSSUxQdEVkRnEydHF2azgwRjEyUnVxeEhEcnpsOFc5Y2h6dzBjbFZqSndJcWtzSlVCR3Rab3BnSFpQa1NiSWRNWXdSSEhIS24rNjl1QWc9PSIsImRldmljZVNpZ25hdHVyZSI6IjFRYXJ0YjkxNW1Cc3habFQ1OE5JVWxXb0dHek94VDI4WUE3NUdHckVDM3U4NlVYMVB3bWF6alZvT3hSaDVEUmd2S3NXclVVMFoyRlU0MjZnc2x6b2dnPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiNTA5NDA4MTIzMzY6MkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJiSmtDZFNRYjc4ZGpGZk8raHZnaEJQTTdzYTlRVE5IU3NrVlVpWkRsN2x2In19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzM2NjMxNjk1LCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUR0VyJ9"
 module.exports = {
   menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || ".",
@@ -51,9 +51,9 @@ module.exports = {
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`BRAND PRODUCT OF 𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨`",
   author: process.env.PACK_AUTHER || " 𝙎-𝙏𝞢𝞜",
-  packname: process.env.PACK_NAME || "𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨",
-  botname: process.env.BOT_NAME || "ST┘𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨𖤐",
-  ownername: process.env.OWNER_NAME || "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑",
+  packname: process.env.PACK_NAME || "petit Prince",
+  botname: process.env.BOT_NAME || "ST┘𝑫𝑹𝑲-𝑹𝑨𝑮N𝑨𖤐",
+  ownername: process.env.OWNER_NAME || "🌹⃟⃢👑 petit Prince 🌹⃟⃢👑",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "tCxobE1FqJgfbDZgsYQNHBFB",

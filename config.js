6fb33a6ab92a08b7786172a3aa328928ca547c1f
petitprince09 <lepetitprince098@gmail.com>
2025-01-11 16:34:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vakp0UnIC
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/vRR63CN.jpeg,https://i.imgur.com/ovCfp9J.jpeg,https://i.imgur.com/edzo5dj.jpeg";
 global.devs = "https://wa.me/50931461936 , https://wa.me/50931461936";
 global.sudo = process.env.SUDO || "50931461936";
-global.owner = process.env.OWNER_NUMBER || "PUT YOUR OWNER NUMBER";
+global.owner = process.env.OWNER_NUMBER || "50940812336";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "true";
@@ -43,15 +43,15 @@ global.scan = "https://drk-tech-2.onrender.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "mettre votre session ici"
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib0tqRzhhaXJLVUNUSXNXYWI5dktMbXFLN0VmclNKSTVnTWJ3N3JtMklIRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK2JtZWZGSndQd1ZPOXE0NHRJbkdFWWZ1MWlIbjJwMjhDVzFzWExQQ0pDMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrQU1XcWtTVWRETXk1VEhINE5PSEJUNFRYWU93NG9rTGdxdUFTbS9LVm1FPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJZRkxiemVjNUR1UGRkY0NvQ2w2Q2l2S21EV1oySFQxdTZ5b25SVisva1VZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkVIcWRvMDl3NlhCRmkwOEpTSU9Ra1NER1ArZUJaZUhMMWR0VSsyV0xWVkU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Imd5Q0l6UGJMTzZRMzZTZ1lqZVJwdlZYbHhhSTBsZE84NnlYemJLNFpJUkU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkh0aDNSeVFMWXdTcHozNEdyd1RxRnFwR25QMUFRU1F3V0UrVkxocG8ycz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMWJXWHlsOXMwYVFRaGErVmttOUdSTHFKTXRoTWtLZzBVN1Z3WU1rTmxWbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJkMW5RZjdNNmphWDJxaXhWVDdIdWtLbHcvR3I4OERPTU5ZVGZVTlQ3Y2ZLUHlpbyttOHIvUU55Yk8wMElYNmpJTHpyRmRQM0dWbkF3dER2cGlLZmdnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NjgsImFkdlNlY3JldEtleSI6IitLTUIxeG9Dc05mYkdvZldmK3ZuQ0lIb3B1b2p3bXhhWHIrUWVabDBKQ1k9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbeyJrZXkiOnsicmVtb3RlSmlkIjoiNTA5NDA4MTIzMzZAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiOUM2M0EwOERCMzYwRkU4OEM1OEZBQzE4RkFENjRGQkUifSwibWVzc2FnZVRpbWVzdGFtcCI6MTczNjYzMDUyNX0seyJrZXkiOnsicmVtb3RlSmlkIjoiNTA5NDA4MTIzMzZAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiMTg1QkEyMzBERUVEMTkzNTdCNUUwOTc2MTFGQjE2QjIifSwibWVzc2FnZVRpbWVzdGFtcCI6MTczNjYzMDUyNX1dLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MSwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sImRldmljZUlkIjoiM3hKU0RQaDRTWnlHWlZJa19ndVN6QSIsInBob25lSWQiOiIzYTBjM2Y0OC0wMWMyLTQ4ZjMtOTI3Zi02ZTJiMmQwYmYwOWMiLCJpZGVudGl0eUlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia2Q2WTFoMDFBYjdjOGw0SlhUUlA1YzY1LytvPSJ9LCJyZWdpc3RlcmVkIjp0cnVlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im5ETjBIUklua2sxdXJhN2dRekwzWml4YytFND0ifSwicmVnaXN0cmF0aW9uIjp7fSwicGFpcmluZ0NvZGUiOiJQRTJLNEMyRiIsIm1lIjp7ImlkIjoiNTA5NDA4MTIzMzY6MUBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiJMZSBQZXRpdCBQcmluY2UifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ05pZmovWUhFT3ZCaTd3R0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InNtUUoxSkJ2dngyTVY4NzZHK0NFRTh6dXhyMUJNMGRLeVJWU0prT1h1Vzg9IiwiYWNjb3VudFNpZ25hdHVyZSI6IkNpZHZPMldaYTU0VzF6Ky9jRFhqM3FQaWIvRmJWSEM0RHphU0RIZ1ZENVFuZitFY3pvSm15NCtIN2xpbWM5cTRNTHVUdTRWb2tlZ2xjTzJWS0NwM0JRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiI0cDNXM0ZmTmFzQWpFSDdpNFRVS2FMUWI0RHJuUFBjd2FUSnpRcW9nblpGb2tXWXRKOU1FcTJRbUMxblBDbE8rU2QwVXUrT0xNVFREZUNNV25pUWppdz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjUwOTQwODEyMzM2OjFAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCYkprQ2RTUWI3OGRqRmZPK2h2Z2hCUE03c2E5UVROSFNza1ZVaVpEbDdsdiJ9fV0sInBsYXRmb3JtIjoic21iYSIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTczNjYzMDUyMSwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFEdFYifQ=="
 module.exports = {
   menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`BRAND PRODUCT OF 𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨`",
-  author: process.env.PACK_AUTHER || " 𝙎-𝙏𝞢𝞜",
-  packname: process.env.PACK_NAME || "𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨",
+  author: process.env.PACK_AUTHER || " prince",
+  packname: process.env.PACK_NAME || "petit-prince",
   botname: process.env.BOT_NAME || "ST┘𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨𖤐",
   ownername: process.env.OWNER_NAME || "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑",
   errorChat: process.env.ERROR_CHAT || "",
